15e4199fe582d99d2d700577dfe46cb5fce12351
hecoding <hlaria@ucm.es>
2014-10-28 03:50:58 +0100

    Populating repo

---
+++ TAIS/prac1/mainej1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include "ColaMedianas.h"
+
+using namespace std;
+
+bool menore(const int& a, const int& b) {
+	return a < b;
+}
+
+bool mayor(const int& a, const int& b) {
+	return a > b;
+}
+
+
+int mainColaMedianas(int argc, char* argv[]) {
+
+	ColaMedianas<int, menore, mayor> cm;
+
+	ifstream fichero("input1.txt");
+	ofstream ficheroSal("output1.txt");
+	int v;
+
+	while(fichero >> v){
+		if (v >= 0) {
+			cm.inserta(v);
+		} else {
+			int med = cm.mediana();
+			cm.quitaMediana();
+			ficheroSal << med << endl;
+		}
+	}
+
+	fichero.close();
+	ficheroSal.close();
+
+	return 0;
+}
+
